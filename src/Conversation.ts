86a98e60164f882c96b68d1f49d4a556b387970f
Yasai <alexvidalcasado@gmail.com>
2023-12-16 12:40:45 +0100

    [ADD] Conversation class

---
+++ src/Conversation.ts
@@ -0,0 +1,36 @@
+import OpenAI from "openai";
+import {
+  ChatCompletionCreateParams,
+  ChatCompletionMessage,
+  ChatCompletionMessageParam,
+} from "openai/resources";
+import { TeasyGPTDefaults } from ".";
+
+export class Conversation {
+  sysMessage: ChatCompletionMessageParam;
+  messages: ChatCompletionMessageParam[] = [];
+
+  defaults: TeasyGPTDefaults;
+  client: OpenAI;
+
+  constructor(sysprompt: string, defaults: TeasyGPTDefaults, client: OpenAI) {
+    this.sysMessage = { content: sysprompt, role: "system" };
+    this.defaults = defaults;
+    this.client = client;
+  }
+
+  async sendMessage(message: string): Promise<ChatCompletionMessage> {
+    this.messages.push({ content: message, role: "user" });
+    const requestBody: ChatCompletionCreateParams = {
+      model: this.defaults.model,
+      messages: [this.sysMessage, ...this.messages],
+      temperature: this.defaults.temperature,
+    };
+
+    const response = await this.client.chat.completions.create(requestBody);
+    const completion = response.choices[0].message;
+    this.messages.push(completion);
+
+    return completion;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import OpenAI from "openai";
+import { Conversation } from "./Conversation";
 
-type TeasyGPTDefaults = {
+export type TeasyGPTDefaults = {
   executeFunc: "auto" | "none";
+  model: string;
+  temperature?: number;
 };
 
 export class easyGPT {
   defaults: TeasyGPTDefaults = {
     executeFunc: "auto",
+    model: "gpt-3.5-turbo-1106",
+    temperature: 0.6,
   };
 
   client: OpenAI;
-  conversations: Record<string, string> = {};
+  conversations: Record<string, Conversation> = {};
   /**
    *
    * @param token OpenAI API token for making requests
    * @param options Default options for the client
    */
   constructor(token: string, options?: TeasyGPTDefaults) {
-    this.defaults = { ...this.defaults, ...options };
+    if (!token) throw new Error("No API token provided");
+    if (options) this.defaults = Object.assign(this.defaults, options);
     this.client = new OpenAI({ apiKey: token });
   }
 }
